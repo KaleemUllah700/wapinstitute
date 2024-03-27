d22c9d8803dd5b808ff4e72737eb75050d46d6dc
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-26 22:08:35 -0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Homepage from "./components/homepage";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </Router>
   )
---
+++ src/components/homepage/index.jsx
@@ -0,0 +1,13 @@
+import Layout from '../shared/layout'
+
+const Homepage = ()=>{
+    return (
+        <Layout>
+            <div>
+                <h1>i am homepage</h1>
+            </div>
+        </Layout>
+    )
+}
+
+export default Homepage;
