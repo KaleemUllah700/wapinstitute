9fae3591617cb81e4ec577d63b6f15069ce284ff
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-30 04:23:34 -0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Homepage from "./components/homepage";
+import Homepage from './components/homepage'
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/homepage" element={<Homepage />} />
         </Routes>
       </Router>
   )
