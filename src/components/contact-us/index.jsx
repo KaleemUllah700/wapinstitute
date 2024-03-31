abd0c3d5002c676b668f241d7d1bf430e70a64ee
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-30 23:34:04 -0700

    contact page completed

--- src/components/contact-us/index.jsx
+++ src/components/contact-us/index.jsx
@@ -1,9 +1,39 @@
 import Layout from '../shared/layout'
 
 const ContactUs = ()=>{
+
+    const contact = [
+        {
+            icon: 'fa fa-phone text-[red]',
+            contact: '(+92) 03019415211'
+        },
+        {
+            icon: 'fa fa-address-book-o text-[blue]',
+            contact: 'District Lakki Marwat, KPK'
+        },
+        {
+            icon: 'fa fa-envelope-o text-[orange]',
+            contact: 'lakki@gmail.com'
+        }
+    ] 
+
     return (
         <Layout>
-            <div>sdfsf</div>
+            <div className='flex items-center justify-center min-h-screen'>
+                    <div className='flex flex-col gap-6 md:w-[35%] w-[90%] h-[290px] bg-[#F7DDF6] rounded-md p-8'>
+                        <h1>Contact Us</h1>
+                        
+                        {
+                            contact.map((cont, index)=>(
+                                <div className='flex items-center gap-6' key={index}>
+                                    <button className='w-[40px] h-[40px] bg-[#FCE1C1] rounded-full'><i className={cont.icon}></i></button>
+                                    <span className='text-lg'>{cont.contact}</span>
+                                </div>
+                            ))
+                        }
+
+                    </div>                    
+            </div>
         </Layout>
     )
 }
