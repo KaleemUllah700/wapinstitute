bcf07c0856cb5b9dc4dab4d8550161cda2c900e5
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 00:46:42 -0700

    certification verification page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Verfication from "./components/verification";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="verified" element={<Verfication />} />
         </Routes>
       </Router>
   )
