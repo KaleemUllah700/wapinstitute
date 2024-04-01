0452006930d39783192a3976ae9cfa90bd024048
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 20:43:40 -0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import Login from "./components/login";
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
   )
---
+++ src/components/login/index.jsx
@@ -0,0 +1,14 @@
+import Layout from '../shared/layout'
+
+const Login = ()=>{
+    return (
+        <Layout>
+            <div>
+                dsdfsdfs
+            </div>
+        </Layout>
+    )
+}
+
+
+export default Login;
