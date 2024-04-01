ccc63397703a9fc3b53fea85b793e55b9bb87b94
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 21:22:19 -0700

    terms and conditions page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import TermsConditions from "./components/terms-conditions";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/terms" element={<TermsConditions />} />
         </Routes>
       </Router>
   )
