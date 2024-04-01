cb9033d163c6984ce7cfa479c486a0410be7280b
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 21:27:10 -0700

    refund policy page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RefundPolicy from "./components/refund-policy";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/refund-policy" element={<RefundPolicy />} />
         </Routes>
       </Router>
   )
