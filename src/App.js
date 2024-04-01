086264a6a8b058e3f65a4e3408d9e773dbd323c8
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 20:39:08 -0700

    signup page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SignUp from "./components/signup";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
   )
