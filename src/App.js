7b3ae67867700b12698ec00afd1520f6e30b6b61
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-03-31 21:16:33 -0700

    privacy page completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "font-awesome/css/font-awesome.min.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Privacy from "./components/privacy";
 
 const App = ()=>{
   return (
       <Router>
         <Routes>
-
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </Router>
   )
