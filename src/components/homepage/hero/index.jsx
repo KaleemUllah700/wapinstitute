46e01b7806aae770c769a78cc3e23e2f0830f839
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-04-01 20:21:19 -0700

    backup

--- src/components/homepage/hero/index.jsx
+++ src/components/homepage/hero/index.jsx
@@ -9,21 +9,21 @@ const Hero = ()=>{
                 <h1 className="md:text-5xl text-xl font-bold">Advanced Diploma</h1>
                 <h1 className="md:text-5xl text-xl font-bold text-[#3DA8E9]">In Software Engineering</h1>
            </div>
-           <div className="bg-[#F9C6F3] md:p-6 p-4 md:w-[530px] w-[350px] h-[400px] rounded-md">
+           <div className="bg-[#F9C6F3] md:p-6 p-4 md:w-[530px] w-[330px] h-[400px] rounded-md">
                 <h1 className="md:text-xl text-lg font-semibold">Apply for admission</h1>
                 <span className="text-md font-semibold opacity-50">(+92) 3019415211</span>
                 <form className="flex flex-col md:gap-4 gap-4 md:text-md text-sm font-semibold">
                     <div>
                         <label>Fullname</label><br />
-                        <input type="text" placeholder="fullname" className="md:w-[450px] w-[320px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
+                        <input type="text" placeholder="fullname" className="md:w-[450px] w-[300px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
                     </div>
                     <div>
                         <label>Email</label><br />
-                        <input type="email" placeholder="email" className="md:w-[450px] w-[320px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
+                        <input type="email" placeholder="email" className="md:w-[450px] w-[300px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
                     </div>
                     <div>
                         <label>Password</label><br />
-                        <input type="password" placeholder="password" className="md:w-[450px] w-[320px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
+                        <input type="password" placeholder="password" className="md:w-[450px] w-[300px] h-[40px] focus:outline-0 bg-[#E4E4E4] pl-2 rounded" />
                     </div>
                     <div>
                         <button type="submit" className="bg-[#911287] p-[10px] rounded text-white text-sm font-semibold">Signup Now</button>
